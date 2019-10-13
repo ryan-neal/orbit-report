48cac5e95339c656fcf3096521664e3b083a2ae5
ryan-neal <ryan.neal0@gmail.com>
2019-10-12 19:03:58 -0500

    added table

--- orbit-report/src/app/app.component.ts
+++ orbit-report/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Satellite } from './satellite';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'orbit-report';
+
+  sourceList: Satellite[];
+
+  constructor() {
+    this.sourceList = [];
+    let satellitesUrl = 'https://api.myjson.com/bins/ccxv5';
+ 
+    window.fetch(satellitesUrl).then(function(response) {
+       response.json().then(function(data) {
+ 
+          let fetchedSatellites = data.satellites;
+          for (let i = 0; i <fetchedSatellites.length; i++){
+            let satellite = new Satellite(fetchedSatellites[i].name, fetchedSatellites[i].type, fetchedSatellites[i].launchDate, fetchedSatellites[i].orbitType, fetchedSatellites[i].operational);
+            this.sourceList.push(satellite)
+          }
+          // TODO: create a Satellite object using new Satellite(fetchedSatellites[i].name, fetchedSatellites[i].type, fetchedSatellites[i].launchDate, fetchedSatellites[i].orbitType, fetchedSatellites[i].operational);
+          // TODO: add the new Satellite object to sourceList using: this.sourceList.push(satellite);
+ 
+       }.bind(this));
+    }.bind(this));
+ 
+ }
 }
